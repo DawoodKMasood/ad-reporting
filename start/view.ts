4e822a9720c812e4152e9b6dec207a975b4c61f0
Dawood Khan Masood <dawoodkmasood@gmail.com>
2025-08-11 15:25:22 +0500

    fix

--- start/view.ts
+++ start/view.ts
@@ -2,6 +2,18 @@ import edge from 'edge.js'
 import env from '#start/env'
 
 /**
- * Define a global property
+ * Define global properties
  */
 edge.global('appUrl', env.get('APP_URL'))
+
+/**
+ * Global helper functions
+ */
+edge.global('formatNumber', (value: number): string => {
+  if (typeof value !== 'number') return '0'
+  return value.toLocaleString()
+})
+
+edge.global('now', () => {
+  return new Date()
+})
